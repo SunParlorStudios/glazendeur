5574e08143c4dec33d0ceef4d4af446f8faf5b49
Riko Ophorst <riko_ophorst@hotmail.com>
2015-02-12 10:28:03 +0100

    - Glorious comments

--- js/utility/state_manager.js
+++ js/utility/state_manager.js
@@ -13,10 +13,25 @@ _.extend(StateManager, {
 	overwrite: false,
 	override: false
 }, {
+	/**
+	 * Holds all states
+	 *
+	 * @private
+	 * @property module:StateManager#_state
+	 * @author Riko Ophorst
+	 */
 	_states: {}
 });
 
 _.extend(StateManager, {
+	/**
+	 * Loads a state from a given json file
+	 *
+	 * @public
+	 * @method module:StateManager#loadState
+	 * @param {string} path - The path to a valid state-formatted JSON file
+	 * @author Riko Ophorst
+	 */
 	loadState: function (path)
 	{
 		var stateData = JSON.load(path);
@@ -58,11 +73,31 @@ _.extend(StateManager, {
 			Log.fatal("Trying to call StateManager.loadState() on a json file with incorrect structuring");
 		}
 	},
+
+	/**
+	 * Loads a save from a given path and adds it to the cache
+	 *
+	 * @public
+	 * @method module:StateManager#loadSave
+	 * @param {string} path - The path to a valid statesave-formatted JSON file
+	 * @author Riko Ophorst
+	 */
 	loadSave: function (path)
 	{
 		var save = JSON.load(path);
 		this._states[save.name] = save;
 	},
+
+	/**
+	 * Switches the current state to given state
+	 *
+	 * @public
+	 * @method module:StateManager#switch
+	 * @param {string} name - The name of the state to switch to
+	 * @param {object} leaveParams - leave parameters which are sent to the left states' leave() method
+	 * @param {object} showParams - show parameters which are sent to the shown states' show() method
+	 * @author Riko Ophorst
+	 */
 	switch: function (name, leaveParams, showParams)
 	{
 		var state;
@@ -90,35 +125,94 @@ _.extend(StateManager, {
 		state.actual.show.call(state.actual, showParams);
 		this._current = state.name;
 	},
+
+	/**
+	 * Attempts to make a save file of given state
+	 *
+	 * @public
+	 * @method module:StateManager#save
+	 * @param {string} name - The state to be saved, can be anything that has been loaded into the StateManager's cache
+	 * @author Riko Ophorst
+	 */
 	save: function ()
 	{
 		//var save = state.serialize();
 	},
+
+	/**
+	 * Updates the current state
+	 *
+	 * @public
+	 * @method module:StateManager#update
+	 * @param {number} dt - deltatime
+	 * @author Riko Ophorst
+	 */
 	update: function (dt)
 	{
 		if (this._current !== undefined)
 			this._states[this._current].actual.update(dt);
 	},
+
+	/**
+	 * FixedUpdates the current state
+	 *
+	 * @public
+	 * @method module:StateManager#fixedUpdate
+	 * @author Riko Ophorst
+	 */
 	fixedUpdate: function ()
 	{
 		if (this._current !== undefined)
 			this._states[this._current].actual.fixedUpdate();
 	},
+
+	/**
+	 * Draws the current state
+	 *
+	 * @public
+	 * @method module:StateManager#draw
+	 * @author Riko Ophorst
+	 */
 	draw: function () 
 	{
 		if (this._current !== undefined)
 			this._states[this._current].actual.draw();
 	},
+
+	/**
+	 * Lets the current state know that given path was reloaded
+	 *
+	 * @public
+	 * @method module:StateManager#reload
+	 * @param {string} path - the path that was reloaded
+	 * @author Riko Ophorst
+	 */
 	reload: function (path)
 	{
 		if (this._current !== undefined)
 			this._states[this._current].actual.reload(path);
 	},
+
+	/**
+	 * Lets the current state know the game is shutting down
+	 *
+	 * @public
+	 * @method module:StateManager#shutdown
+	 * @author Riko Ophorst
+	 */
 	shutdown: function ()
 	{
 		if (this._current !== undefined)
 			this._states[this._current].actual.shutdown();
 	},
+	
+	/**
+	 * Retrieves the current state
+	 *
+	 * @public
+	 * @method module:StateManager#current
+	 * @author Riko Ophorst
+	 */
 	current: function ()
 	{
 		return this._states[this._current].actual;
